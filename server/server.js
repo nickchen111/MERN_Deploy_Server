532addcf377adef665c40214cd6c4fa98ba723a9
nick chen <s27206383@gmail.com>
2024-04-08 02:06:53 +0800

    initial commit

--- server/server.js
+++ server/server.js
@@ -8,10 +8,12 @@ const courseRoute = require("./routes").course;
 const passport = require("passport");
 require("./config/passport")(passport);
 const cors = require("cors");
+const path = require("path");
+const port = process.env.PORT || 8080; // HEROKU自動動態設定的
 
 // 連結mongoDB
 mongoose
-  .connect("mongodb://localhost:27017/mernDB")
+  .connect(process.env.MONGODB_CONNECTION) // "mongodb://localhost:27017/mernDB"本地的話
   .then(() => {
     console.log("Connecting to mongodb...");
   })
@@ -23,6 +25,7 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.use("/api/user", authRoute);
 // courseRoute 應該被jwt保護
@@ -33,9 +36,17 @@ app.use(
   courseRoute
 );
 
+//將網頁部署到雲端伺服器後 如果有接收到除了上面兩個的URL的話 就會導向下面的
+// production staging 都是部署上去後會有的值 NODE_ENV會自動建立
+if (process.env.NODE_ENV == "production" || process.env.NODE_ENV == "staging") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 //只有登入系統的人才能夠去新增課程或是註冊課程
 //驗證jwt token
 
-app.listen(8080, () => {
+app.listen(port, () => {
   console.log("後端伺服器聆聽port 8080");
 });
